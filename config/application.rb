3e5558088e5640b6b38188402eafc854ffae1307
Neil Ni <tzuyangni@gmail.com>
2017-03-10 08:25:34 -0500

    add cors origin option

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,12 @@ module Companyserver
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    if Rails.env.development?
-      config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins do |source, env|
+          source.include?(Rails.application.config.x.cors_origin)
         end
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
   end
